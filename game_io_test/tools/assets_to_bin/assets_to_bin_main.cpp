ebbc758d0a98b6dac53346c951d2cf8a93a01257
adam-lafontaine <adam.lafontiane@gmail.com>
2025-04-09 12:42:43 -0400

    cleanup tools

--- game_io_test/tools/assets_to_bin/assets_to_bin_main.cpp
+++ game_io_test/tools/assets_to_bin/assets_to_bin_main.cpp
@@ -6,23 +6,6 @@ constexpr auto SRC_ROOT = "/home/adam/Repos/SDL3Game/game_io_test/assets";
 constexpr auto DST_ROOT = "/home/adam/Repos/SDL3Game/game_io_test/src/res";
 
 
-u8 to_mask(p32 p)
-{
-    if (!p.alpha) // transparent
-    {
-        return 0;
-    }
-
-    auto sum = p.red + p.green + p.blue;
-    if (!sum) // black
-    {
-        return 1;
-    }
-
-    return 2;
-}
-
-
 int main()
 {
     auto root = fs::path(SRC_ROOT);
@@ -30,8 +13,7 @@ int main()
     a2b::AssetFiles af;
 
     a2b::create(af, DST_ROOT, "io_test_data");
-
-    //a2b::append_image_dir(af, root / "masks", to_mask);
+    
     a2b::append_file_dir(af, root / "masks");
     a2b::append_file_dir(af, root / "music");
     a2b::append_file_dir(af, root / "sfx");
