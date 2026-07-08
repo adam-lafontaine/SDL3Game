d97e1db3430b1f4fabba9198c524b5105277785d
adam-lafontaine <adam.lafontaine@gmail.com>
2026-07-08 05:49:58 -0400

    rename old libs

--- libs_old/alloc_type/alloc_type.hpp
+++ libs_old/alloc_type/alloc_type.hpp
@@ -2,6 +2,12 @@
 
 #include "../util/types.hpp"
 
+// for testing
+/*#ifndef ALLOC_COUNT
+#define ALLOC_COUNT
+#endif*/
+
+
 namespace mem
 {    
     void* alloc_memory(u32 n_elements, u32 element_size, cstr tag);
@@ -54,3 +60,52 @@ namespace mem
         untag_memory((void*)ptr, (u32)sizeof(T));
     }
 }
+
+
+#ifdef ALLOC_COUNT
+
+namespace mem
+{ 
+
+    struct AllocationStatus
+    {
+        static constexpr u32 MAX_SLOTS = 50;
+
+        cstr type_name = 0;
+        u32 element_size = 0;
+        u32 max_allocations = 0;
+
+        u32 bytes_allocated = 0;
+        u32 elements_allocated = 0;
+
+        u32 n_allocations = 0;
+
+        // TODO: max allocations
+        cstr slot_tags[MAX_SLOTS] = { 0 };
+        u32 slot_sizes[MAX_SLOTS] = { 0 };
+    };
+
+
+    struct AllocationHistory
+    {
+        cstr type_name = 0;
+        u32 element_size = 0;
+        u32 max_allocations = 0;
+
+        u32 n_items = 0;
+
+        cstr* tags = 0;
+        cstr* actions = 0;
+        u32* sizes = 0;
+        u32* n_allocs = 0;
+        u32* n_bytes = 0;
+
+    };
+
+
+    AllocationStatus query_status(u32 element_size);
+
+    AllocationHistory query_history(u32 element_size);
+}
+
+#endif
