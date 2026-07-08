a22286749338c01d0a75aa789d7239be4dc56f4f
adam-lafontaine <adam.lafontaine@gmail.com>
2026-07-08 07:01:44 -0400

    remove makefile dependencies

--- game_io_test/src/pltfm/sdl2/main_o.cpp
+++ game_io_test/src/pltfm/sdl2/main_o.cpp
@@ -17,8 +17,8 @@
 #include "../../../../libs/sdl2/sdl_audio.cpp"
 #include "../../../../libs/sdl2/sdl_filesystem.cpp"
 #include "../../../../libs/sdl2/sdl_stb_libs.cpp"
-#include "../../../../libs/math/math.cpp"
 
+#include "../../../../libs/math/math.cpp"
 #include "../../../../libs/datetime/datetime.cpp"
 
 #ifdef NDEBUG
