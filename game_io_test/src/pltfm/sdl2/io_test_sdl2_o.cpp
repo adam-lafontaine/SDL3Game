d71e7b65085df254caa313be004f029c26fa15ce
adam-lafontaine <adam.lafontaine@gmail.com>
2026-07-09 14:31:46 -0400

    sdl2 wasm

--- game_io_test/src/pltfm/sdl2/io_test_sdl2_o.cpp
+++ game_io_test/src/pltfm/sdl2/io_test_sdl2_o.cpp
@@ -9,12 +9,18 @@
 #include "../../../../libs/sdl2/sdl_alloc.cpp"
 #include "../../../../libs/sdl2/sdl_input.cpp"
 #include "../../../../libs/sdl2/sdl_window.cpp"
-#include "../../../../libs/sdl2/sdl_audio.cpp"
-#include "../../../../libs/sdl2/sdl_filesystem.cpp"
 #include "../../../../libs/sdl2/sdl_stb_libs.cpp"
 
 #include "../../../../libs/math/math.cpp"
 #include "../../../../libs/datetime/datetime.cpp"
+
+#ifndef NO_AUDIO
+#include "../../../../libs/sdl2/sdl_audio.cpp"
+#endif
+
+#ifndef NO_FILESYSTEM
+#include "../../../../libs/sdl2/sdl_filesystem.cpp"
+#endif
 
 #ifdef NDEBUG
 #include "../../../src/app/app.cpp"
